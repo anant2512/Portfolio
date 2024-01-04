14b5877fd0ec3cb394633319051924d564e874d7
anant2512 <155611758+anant2512@users.noreply.github.com>
2024-01-04 11:42:36 +0530

    Add files via upload

---
+++ Ap.js
@@ -0,0 +1,22 @@
+const container = document.querySelector('.container');
+
+const cloneContainer = container.cloneNode(true);
+cloneContainer.id = 'dark-container';
+document.body.appendChild(cloneContainer);
+cloneContainer.classList.remove('active');
+const toggleIcons = document.querySelectorAll('.toggle-icon');
+const icons = document.querySelectorAll('.toggle-icon i');
+const darkContainer = document.querySelector('#dark-container');
+const darkContainerImg = document.querySelector('#dark-container .home-img img');
+darkContainerImg.src = 'Anant-removebg-preview (1).png';
+
+toggleIcons.forEach(toggle => {
+      toggle.addEventListener('click', () => {
+
+        icons.forEach(icon => {
+            icon.classList.toggle('bx-sun');
+        });
+        container.classList.toggle('active');
+        darkContainer.classList.toggle('active');
+      });
+});
